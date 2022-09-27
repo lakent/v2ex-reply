d1f6aa9e3440886ec1dd09552d70331d2a5b6880
lakent <Royal_Hugh@163.com>
2022-09-27 19:47:19 +0800

    Update v2ex-reply.user.js

--- v2ex-reply.user.js
+++ v2ex-reply.user.js
@@ -13,41 +13,44 @@
 
 (function () {
     'use strict';
+    function getContenes(x) {
+        return Array.from(x, i => i.innerText)
+    }
 
-    var replyList = document.querySelectorAll("div.cell table tbody tr td:nth-child(3) div.reply_content")
-    var replyContents = Array.from(replyList, i => i.innerText)
-    var topReplyindex = Number(document.querySelector(".no").innerText);
+    let replyList = document.querySelectorAll("div.cell table tbody tr td:nth-child(3) div.reply_content")
+    const userList = document.querySelectorAll("div.cell table tbody tr td:nth-child(3) strong a")
+    const commentList = document.querySelectorAll("#Main div:nth-child(4) div[id].cell");
 
-    var userList = document.querySelectorAll("div.cell table tbody tr td:nth-child(3) strong a")
-    var users = Array.from(userList, i => i.innerText)
+    const replyContents = getContenes(replyList)
+    const users = getContenes(userList)
+    const topReplyindex = Number(document.querySelector(".no").innerText);
+    const indexRegex = RegExp(/\s#(\d+)/, 'g');
 
-    var commentList = document.querySelectorAll("#Main div:nth-child(4) div[id].cell");
+    let indexs = new Array();
 
-    let i = 1;
-    const indexRegex = /\s#(\d+)/g;
-    while (i < commentList.length) {
-        if (replyContents[i].match("楼上")) {
-            replyList[i - 1].appendChild(commentList[i]);
-        }
-
-        if (replyContents[i].match(indexRegex)) {
-            var indexs = [...replyContents[i].matchAll(indexRegex)];
+    for (let i = 1; i < commentList.length; i++) {
+        indexs = [...replyContents[i].matchAll(indexRegex)];
+        if (indexs.length) {
             for (const index of indexs) {
                 if (index[1] - topReplyindex > 0) {
                     var commentClone = commentList[i].cloneNode(true);
                     replyList[index[1] - topReplyindex].appendChild(commentClone);
-                    commentList[i].remove();
+
                 }
             }
+            commentList[i].remove();
+            continue;
+        }
 
-        } else {
-            for (var j = i - 1; j >= 0; j--) {
-                if (replyContents[i].match(users[j])) {
-                    replyList[j].appendChild(commentList[i]);
-                    break;
-                }
+        if (replyContents[i].match("楼上")) {
+            replyList[i - 1].appendChild(commentList[i]);
+        }
+
+        for (var j = i - 1; j >= 0; j--) {
+            if (replyContents[i].match(users[j])) {
+                replyList[j].appendChild(commentList[i]);
+                break;
             }
         }
-        i++;
     }
 })();
